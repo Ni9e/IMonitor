e0b64611c9d7d5c6b399ac1e046dbfb0b55bf3ea
Finkle <sikong1986@gmail.com>
2014-01-09 22:34:25 +0800

    Create Project
    Include monitor service and web

---
+++ IMonitorService/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMonitorService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
